9d672c29eac0f86617901f0da9625221293fac0b
batuchek68-ux <batuchek68@gmail.com>
2026-06-15 18:21:51 +0800

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,8 @@
+async function load() {
 
+  const res = await fetch("http://localhost:8000/run");
+  const data = await res.json();
+
+  document.getElementById("output").innerText =
+    JSON.stringify(data, null, 2);
+}
