117bd2e4f2c6aba9ec874f56014dc9538c5559c1
batuchek68-ux <batuchek68@gmail.com>
2026-06-15 18:56:55 +0800

    Update auth.py

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -1,1 +1,13 @@
+import jwt
+from datetime import datetime, timedelta
 
+SECRET = "supersecret"
+
+def create_token(user_id: int):
+
+    payload = {
+        "user_id": user_id,
+        "exp": datetime.utcnow() + timedelta(days=1)
+    }
+
+    return jwt.encode(payload, SECRET, algorithm="HS256")
