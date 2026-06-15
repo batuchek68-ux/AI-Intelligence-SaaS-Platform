3d77ecf19b379c1d2262a70de3f85cd6a134d23f
batuchek68-ux <batuchek68@gmail.com>
2026-06-15 18:29:50 +0800

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,1 +1,10 @@
+import jwt
+from datetime import datetime, timedelta
 
+def create_token(data):
+    payload = {
+        "data": data,
+        "exp": datetime.utcnow() + timedelta(hours=24)
+    }
+
+    return jwt.encode(payload, "SECRET", algorithm="HS256")
